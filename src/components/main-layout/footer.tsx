8b52b0140d0ce51790fa034bef014a3ac3fce77b
BorovChe <roman2000borov@gmail.com>
2025-07-21 22:34:04 +0300

    fix footer

--- src/components/main-layout/footer.tsx
+++ src/components/main-layout/footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <footer className="relative flex items-center min-h-[68px]  border-[--footer-border-cl-30] border-t-[0.5px] border-solid text-[--main-dark-cl]">
       <Container>
-        <Logo textColor="--main-dark-cl" />
-        <I18nSwitcher textColor="--main-dark-cl" />
+        <Logo textColor="--main-dark-cl" textSize="18px" />
+        <I18nSwitcher textColor="--main-dark-cl" textSize="14px" />
       </Container>
     </footer>
   );
